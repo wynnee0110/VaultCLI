af36198083390efeb555f592ebd01041ac3b1ea9
Wayne <wynnee0110@gmail.com>
2026-05-04 01:48:23 +0800

    sd

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ OS="$(uname -s)"
 
 if [ "$OS" = "Linux" ]; then
 URL="https://github.com/wynnee0110/VaultCLI/releases/download/v1.0.0/vault-linuxV1"
-CHECKSUM_URL="https://github.com/wynnee0110/VaultCLI/releases/download/v1.0.0/vault-linuxV1.sha256"
+
 else
   echo "❌ Only Linux is supported right now"
   exit 1
@@ -15,7 +15,7 @@ fi
 
 TMP_DIR=$(mktemp -d)
 TMP_FILE="$TMP_DIR/vault"
-CHECKSUM_FILE="$TMP_DIR/vault.sha256"
+
 
 cleanup() {
   rm -rf "$TMP_DIR"
@@ -24,19 +24,7 @@ trap cleanup EXIT
 
 echo "⬇️ Downloading VaultCLI..."
 curl -LfsSL "$URL" -o "$TMP_FILE"
-curl -LfsSL "$CHECKSUM_URL" -o "$CHECKSUM_FILE"
 
-echo "🔎 Verifying checksum..."
-
-cd "$TMP_DIR"
-
-EXPECTED_HASH=$(cat vault.sha256)
-ACTUAL_HASH=$(sha256sum vault | awk '{print $1}')
-
-if [ "$EXPECTED_HASH" != "$ACTUAL_HASH" ]; then
-  echo "❌ Checksum verification failed!"
-  exit 1
-fi
 
 chmod +x "$TMP_FILE"
 sudo mv "$TMP_FILE" /usr/local/bin/vault
